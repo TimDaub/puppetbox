094f08d9d6d67bb5817d3856f88f5c590232ab7d
Tim Daubenschütz <tim@daubenschuetz.de>
2021-02-04 18:06:10 +0100

    try equalizing rendering

--- test/index_test.mjs
+++ test/index_test.mjs
@@ -7,56 +7,17 @@ import imgur from "imgur";
 
 import { capture, htmlEnvelope } from "../src/index.mjs";
 
-const randString = () =>
-  Math.random()
-    .toString(36)
-    .substring(7);
-
-test.skip("if screenshots that were rendered on different OS can pass looksSame by ignoring anti aliasing", t => {
-  const mac = "test/fixtures/helloworld.png";
-  const githubAction = "test/fixtures/helloworld_ghaction.png";
-  t.plan(1);
-  return new Promise(res => {
-    looksSame(
-      mac,
-      githubAction,
-      { ignoreAntialiasing: true, antialiasingTolerance: 3 },
-      (err, { equal }) => {
-        t.true(equal);
-        res();
-      }
-    );
-  });
-});
-
-test("if screenshots that were rendered on different OS will fail looksSame by comparing anti aliasing", t => {
-  const mac = "test/fixtures/helloworld.png";
-  const githubAction = "test/fixtures/helloworld_ghaction.png";
-  t.plan(1);
-  return new Promise(res => {
-    looksSame(
-      mac,
-      githubAction,
-      { ignoreAntialiasing: false },
-      (err, { equal }) => {
-        t.false(equal);
-        res();
-      }
-    );
-  });
-});
-
-test("if capturing an image works", async t => {
-  const input = htmlEnvelope(`<p>hello world</p>`);
+test.skip("bla", async t => {
+  const input = htmlEnvelope(`<p style="font-family: Arial;">hello world</p>`);
 
   const screenshot = await capture(input, {
     width: 100,
     height: 100
   });
 
-  const ssPath = `${tempDir}/${randString()}.png`;
+  const ssPath = `${tempDir}/testfile.png`;
   writeFileSync(ssPath, screenshot);
-  const expectedFilePath = "test/fixtures/helloworld.png";
+  const expectedFilePath = "test/fixtures/helloworld_arial_github.png";
 
   t.plan(3);
   t.true(existsSync(ssPath));
@@ -80,43 +41,37 @@ test("if capturing an image works", async t => {
   });
 });
 
-test("that matching two renders from two different OS is possible", async t => {
-  const input = htmlEnvelope(`<p style="font-family: Arial;">hello world</p>`);
+test("if capturing an image works", async t => {
+  const input = htmlEnvelope(`<p>hello world</p>`);
 
   const screenshot = await capture(input, {
     width: 100,
     height: 100
   });
 
-  const ssPath = `${tempDir}/${randString()}.png`;
+  const ssPath = `${tempDir}/testfile.png`;
   writeFileSync(ssPath, screenshot);
-  const expectedFilePath = "test/fixtures/helloworld_arial.png";
+  const expectedFilePath = "test/fixtures/helloworld.png";
 
   t.plan(3);
   t.true(existsSync(ssPath));
   t.true(existsSync(expectedFilePath));
   return new Promise(res => {
-    looksSame(
-      ssPath,
-      expectedFilePath,
-      { ignoreAntialiasing: true },
-      async (err, { equal }) => {
-        if (!equal) {
-          // NOTE: This section is mainly to debug the tests on GitHub where
-          // apparently images are rendered differently than on e.g. my local
-          // computer.
-          const actualUL = await imgur.uploadFile(ssPath);
-          const expectedUL = await imgur.uploadFile(expectedFilePath);
-
-          console.info(`
+    looksSame(ssPath, expectedFilePath, async (err, { equal }) => {
+      if (!equal) {
+        // NOTE: This section is mainly to debug the tests on GitHub where
+        // apparently images are rendered differently than on e.g. my local
+        // computer.
+        const actualUL = await imgur.uploadFile(ssPath);
+        const expectedUL = await imgur.uploadFile(expectedFilePath);
+        console.info(`
             Actual: ${actualUL.data.link}
             Expected: ${expectedUL.data.link}
           `);
-        }
-        t.true(equal);
-        res();
       }
-    );
+      t.true(equal);
+      res();
+    });
   });
 });
 
